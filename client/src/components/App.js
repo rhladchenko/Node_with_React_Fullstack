fcd53e21b534423857ae2532af1073a47018e19a
Ruslan Hladchenko <ruslan.hladchenko@cpcs.ws>
2023-12-28 09:06:45 +0200

    Added Route.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,21 @@
-// import styles from './App.module.scss';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 export default function App() {
-	return <div>Hi, There</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path='/' component={Landing} exact />
+					<Route path='/dashboard' component={Dashboard} />
+					<Route path='/survey' component={SurveyNew} />
+					<Route path='/header' component={Header} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
