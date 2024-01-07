9e346bb6eee3f99aaa227252f6bfeb351132e9dd
Ruslan Hladchenko <ruslan.hladchenko@cpcs.ws>
2024-01-07 14:16:08 +0200

    Added Login and Llogout.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,27 @@
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+
+			case false:
+				return (
+					<li>
+						<a href='/auth/google'>Auth with Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href='/api/logout'>Logout</a>
+					</li>
+				);
+		}
+	}
 	render() {
 		return (
 			<nav>
@@ -9,13 +29,17 @@ export default class Header extends Component {
 					<Link to='/' className='left brand-logo'>
 						Emaly
 					</Link>
-					<ul className='right'>
-						<li>
-							<a href='sass.html'>Login with Google</a>
-						</li>
-					</ul>
+					<ul className='right'>{this.renderContent()}</ul>
 				</div>
 			</nav>
 		);
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		auth: state.auth,
+	};
+}
+
+export default connect(mapStateToProps)(Header);
