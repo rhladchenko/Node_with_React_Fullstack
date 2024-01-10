b6902edc79d0360f94f547c760a46d4db603de2a
Ruslan Hladchenko <ruslan.hladchenko@cpcs.ws>
2024-01-11 00:10:34 +0200

    fixing versions

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
-// 1) Import ReactDOM library
+import 'materialize-css/dist/css/materialize.min.css';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-// 2) Import App component as usual
 import App from './components/App';
+import reducers from './reducers';
 
-// 3) Get a reference to the div with ID root
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
 const el = document.getElementById('root');
-
-// 4) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-// 5) Show the component on the screen
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
+console.log('Environment is', process.env.NODE_ENV);
