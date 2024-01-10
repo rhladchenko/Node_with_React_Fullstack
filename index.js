48728633c1d83002a58d57ccc4eb88b0808c81ef
Ruslan Hladchenko <ruslan.hladchenko@cpcs.ws>
2024-01-10 09:11:50 +0200

    Removedd body-parser.

--- index.js
+++ index.js
@@ -10,18 +10,12 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
 		keys: [keys.cookieKey],
 	})
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
