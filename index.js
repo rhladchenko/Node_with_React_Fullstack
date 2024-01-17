fa0ca320ff640d28bf5760175bb049fc80ecb867
Ruslan Hladchenko <ruslan.hladchenko@cpcs.ws>
2024-01-17 09:36:38 +0200

    Added billing and client side app.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
+const path = require('path');
 require('./models/User');
 require('./services/passport');
 
@@ -24,6 +25,14 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
