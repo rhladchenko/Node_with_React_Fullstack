1c2fcb50962876ffcfeb3c0638de4d32be82ea62
Ruslan Hladchenko <ruslan.hladchenko@cpcs.ws>
2024-01-22 20:50:56 +0200

    Added Mailer class.

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,4 +2,14 @@ const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends Mailer.Msil {}
+class Mailer extends helper.Mail {
+	constructor({ subject, recipients }, content) {
+		super();
+		this.from_email = new helper.Email('gladchenkoruslan@gmail.com');
+		this.subject = subject;
+		this.body = new helper.Content('text/html', content);
+		this.recipients = formatAddresses(recipients);
+	}
+}
+
+module.exports = Mailer;
