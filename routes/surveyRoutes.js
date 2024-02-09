bbf63469c94530845f7b69eadb37507429ac3b93
Ruslan Hladchenko <ruslan.hladchenko@cpcs.ws>
2024-02-09 09:00:15 +0200

    Added webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
+
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,8 +11,30 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-	app.get('api/surveys/thanks', (req, res) => {
+	app.get('/api/surveys/thanks', (req, res) => {
 		res.send('Thanks for voting!');
+	});
+
+	app.post('/api/surveys/webhooks', (req, res) => {
+		const p = new Path('/api/surveys/:surveyId/:choice');
+
+		const events = _.chain(req.body)
+			.map(({ email, url }) => {
+				const match = p.test(new URL(url).pathname);
+
+				if (match) {
+					return {
+						email,
+						surveyId: match.surveyId,
+						choice: match.choice,
+					};
+				}
+			})
+			.compact()
+			.uniqBy('email', 'surveyId')
+			.value();
+
+		console.log('::: surveyRoutes >> events: ', events);
 	});
 
 	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
