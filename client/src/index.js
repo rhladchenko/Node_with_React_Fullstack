29e10f10be5f62f8ff4df68aceddc3b53cb75f7a
Ruslan Hladchenko <ruslan.hladchenko@cpcs.ws>
2024-02-14 10:52:23 +0200

    Fixed deploy bugs.

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 import axios from 'axios';
 window.axios = axios;
 
-const store = createStore(reducers, {}, applyMiddleware(thunk));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 const el = document.getElementById('root');
 
